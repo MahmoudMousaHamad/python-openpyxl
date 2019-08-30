f958b8d609ec3a9346a2bc752ae104724e638848
Mousa Hamad <mahmoudhamad@ou.edu>
2019-08-30 17:48:34 -0500

    init

---
+++ script.py
@@ -0,0 +1,57 @@
+from openpyxl import load_workbook
+import re
+import base64
+
+class Pricing:
+    def __init__(self, code, encoded_code, title, kind, price):
+        self.code = code
+        self.encoded_code = encoded_code
+        self.title = title
+        self.kind = kind
+        self.price = price
+    
+    def generate_anchor_element(self):
+        if self.kind == "CEO":
+            return f"<a href='/entry-form-executives/?sub_sector={self.title}&code={self.encoded_code}'>{self.title}</a>"
+        else:
+            return f"<a href='/entry-form-organizations/?sub_sector={self.title}&code={self.encoded_code}'>{self.title}</a>"
+
+    def generate_case_statement(self):
+        return f"case: '{self.code}': \n price = {self.price}; \n break; \n"
+
+
+
+wb = load_workbook("Workbook.xlsx")
+
+sheet = wb.active
+
+max_row = sheet.max_row
+max_col = sheet.max_column
+
+pricings = []
+
+# row
+for i in range(1, max_row + 1):
+    row_data = []
+    is_pricing_row = False
+    # column
+    for j in range(1, max_col + 1):
+        cell = sheet.cell(row = i, column = j)
+        cell_val = cell.value
+        if cell_val:
+            regex = re.compile(r'^[A-Z][0-9]{1,2}?$')
+            if regex.search(cell_val):
+                is_pricing_row = True
+            if is_pricing_row:
+                row_data.append(cell_val.strip())
+            print(cell_val.strip(), end = " ")
+    if is_pricing_row:
+        code = row_data[0]
+        encoded_code = base64.b64encode(code.encode("utf-8"))
+        title = row_data[1]
+        kind = 'CEO' if 'CEO' in title else 'ORG'
+        price = row_data[2][3:]
+        pricing = Pricing(code, encoded_code, title, kind, price)
+        pricings.append(pricing)
+        print(pricing.generate_anchor_element())
+    print('\n')
